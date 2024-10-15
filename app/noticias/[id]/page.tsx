44822643344b05f873af051f71096bb0b49ff9be
ramiro-uziel <ramirouziel15@gmail.com>
2024-10-14 19:18:05 -0600

    Cambios a texto de news page.

--- app/noticias/[id]/page.tsx
+++ app/noticias/[id]/page.tsx
@@ -52,7 +52,7 @@ const Page: React.FC = () => {
   if (!currentNews)
     return (
       <div className="flex justify-center items-center h-screen">
-        Loading...
+        Cargando...
       </div>
     );
 
@@ -102,7 +102,7 @@ const Page: React.FC = () => {
         Ver más Noticias:
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5 pb-10">
         {otherNews.map((article) => (
           <div
             key={article._id}
